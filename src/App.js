420c32e80eb6583b9870bf7de01e6b44b4b3b8a5
Ashwanikumar27 <ak2471016@gmail.com>
2017-05-19 11:41:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import './index.css';
 import $ from 'jquery';
-import WeatherValuesComponent from './WeatherValuesComponent.js';
+import MovieDisplay from './MovieDisplay.js';
+
 
 
 class App extends Component 
@@ -9,46 +11,53 @@ class App extends Component
   constructor()
   {
     super();
-    this.state={"name":"","weather":[],"tb":false};
+    this.state={"name":"","movie":[],"tb":false};
     this.handleChange=this.handleChange.bind(this);
-    this.fetchWeatherValue=this.fetchWeatherValue.bind(this);
+    this.getmovieValue=this.getmovieValue.bind(this);
   }
 
   handleChange(e)
   {
     this.setState({name: e.target.value});
   }
-  fetchWeatherValue(cityname) 
+  getmovieValue(moviename) 
   {
-    console.log(cityname);
+
+  var Input = moviename;
+        var pathapi = 'http://www.omdbapi.com/?s='+ Input;
     this.setState({tb: true});
     $.ajax({
     
-    url: "http://api.openweathermap.org/data/2.5/forecast?q="+cityname+"&appid=e3ecc901824c48cabe7f3a3a01e6defb",
+    url: pathapi,
     type: "GET",
     crossDomain: true,
     dataType: 'JSON',
 
     success : function(msg){
-    console.log(msg.list);
-    this.setState({weather:msg.list});
+    console.log(msg.Search);
+    this.setState({movie:msg.Search});
+  
     }.bind(this),
     error: function(err){
     console.log("Main-Error Fetching ");
     }
     });
   }
+  
+
+
+
   render() {
     return (
       <div className="App">
-      <h1>Weather Forecast</h1>
-        <input type="text" value={this.state.name} onChange={this.handleChange}/>
-        
-        <button type="submit" onClick={() => {this.fetchWeatherValue(this.state.name)}}>Search</button>
-        <WeatherValuesComponent weather={this.state.weather} tb={this.state.tb}/>
-      </div>
+            <input id="search" type="text" value={this.state.name} onChange={this.handleChange} placeholder="Search Your movie"/>
+            <button type="submit"  id="btn" onClick={() => {this.getmovieValue(this.state.name)}}>Search</button>
+            <MovieDisplay movie={this.state.movie} tb={this.state.tb} />
+
+        </div>
     );
   }
 }
 
 export default App;
+ 
